e8b566ba367d35557373c2405f2315d3b0b93cd3
Aaban Irfan <aabani@uia.no>
2024-02-13 11:40:00 +0100

    Configure

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,21 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('studentassessmentdashboa-23340-firebase-adminsdk-uuoh3-7d9e57ca19.json.json');
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD1UrKo3tkLcAvM0PIK8eNVEJlCttEg6GI",
+    authDomain: "studentassessmentdashboa-23340.firebaseapp.com",
+    projectId: "studentassessmentdashboa-23340",
+    storageBucket: "studentassessmentdashboa-23340.appspot.com",
+    messagingSenderId: "382351852374",
+    appId: "1:382351852374:web:015d481b1b5e3933858cf7",
+    measurementId: "G-MJQH7M305F"
+};
 
-const db = admin.firestore();
-
-module.exports = { db };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
